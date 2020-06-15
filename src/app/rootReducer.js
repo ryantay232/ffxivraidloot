e8e7afaddb5c3a9c32a5559627332b44c910d340
Ryan Tay <54162800+ryantay232@users.noreply.github.com>
2020-06-15 17:18:02 +0800

    removed counter

--- src/app/rootReducer.js
+++ src/app/rootReducer.js
@@ -1,9 +1,7 @@
 import { combineReducers } from '@reduxjs/toolkit';
-import counterReducer from "../features/counter/counterSlice";
 import memberReducer from "../components/member/memberSlice";
 
 const rootReducer = combineReducers({
-  counter: counterReducer,
   member: memberReducer,
 });
 
